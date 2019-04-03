d430973984aff19516783c049eea39b3983fddd6
Sam <samrtomkinson@gmail.com>
2019-04-03 19:20:48 +0100

    Initial Vue site

---
+++ src/components/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="jumbotron">
+        <h1>Git Grapher</h1>
+        <p class="lead">Build your git graph below</p>
+        <hr class="my-4">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
