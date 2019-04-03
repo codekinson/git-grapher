fa73635ac208e5b17b7db73cd612b7fbba8b3135
Sam <samrtomkinson@gmail.com>
2019-04-03 19:20:48 +0100

    Initial Vue site

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   <Banner /> 
+  </div>
+</template>
+
+<script>
+import Banner from './components/Banner.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Banner
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 3em;
+}
+</style>
